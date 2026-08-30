a6a5c8f9c4053c174781270782dc74dd72d356ba
Moch Adriq <mochfadillah1208@gmail.com>
2026-08-30 20:22:09 +0700

    Fix local DB connection by making SSL CA conditional on production environment

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => extension_loaded('pdo_mysql') && env('APP_ENV') === 'production' ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA', base_path('cacert.pem')),
             ]) : [],
         ],
